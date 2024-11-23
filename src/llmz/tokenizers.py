fd7f99e6dd8a1324aa7628efacae5c705a1e0cd5
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-11-23 13:51:54 +0000

    Add GPT tokenizer class and test

--- src/llmz/tokenizers.py
+++ src/llmz/tokenizers.py
@@ -1,18 +1,39 @@
 """Tokenizers for LLMs."""
 
+import tiktoken
+import torch
 from torch.utils.data import Dataset
 
 
 class GPTSmallTextDataset(Dataset):
-    """GPT dataset interface for any 'small' text data."""
+    """GPT dataset interface for any 'small' text data.
 
-    def __init__(self, text: str, max_length: int, *, stride: int = 1):
+    This will tokenize all text in-memory using a GPT2's tokenization algorithm, which
+    is a pre-trained Bite Pair Encoding (BPE).
+    """
+
+    def __init__(self, text: str, max_length: int, stride: int = 1):
         """Initialise.
 
         Args:
-            text: TODO
-            max_length: TODO
-            stride: TODO
+            text: Raw text data to convert into tokens.
+            max_length: Number of tokens for each data instance.
+            stride: Separation (in tokens) between consecutive instances. Defaults to 1.
 
         """
-        pass
+        tokenizer = tiktoken.get_encoding("gpt2")
+        tokens = tokenizer.encode(text)
+        n_instances = len(tokens) - max_length
+
+        self._X = torch.ones((n_instances, max_length))
+        self._y = torch.ones((n_instances, max_length))
+
+        for i in range(0, n_instances, stride):
+            self._X[i,] = torch.tensor(tokens[i : i + max_length])
+            self._y[i,] = torch.tensor(tokens[i + 1 : i + max_length + 1])
+
+    def __len__(self) -> int:
+        return self._X.size(0)
+
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        return self._X[idx,], self._y[idx,]
--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -1,8 +1,30 @@
 """Test for tokenizers."""
 
+import tiktoken
+
 from llmz.tokenizers import GPTSmallTextDataset
 
 
 def test_GPTDataset_encodes_data():
-    text = "Attacks ship off the shoulder of Orion."
-    dataset = GPTSmallTextDataset(text, 3, 1)
+    text = "Attacks ships off the shoulder of Orion."
+    max_len = 3
+    stride = 1
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    exp_tokens = tokenizer.encode(text)
+
+    dataset = GPTSmallTextDataset(text, max_len, stride=stride)
+    X_0, y_0 = dataset[0]
+    X_1, y_1 = dataset[1]
+
+    assert len(dataset) == len(range(0, len(exp_tokens) - max_len, 1))
+    assert len(X_0) == len(y_0) == max_len
+
+    assert X_0[1] == y_0[0]
+    assert X_1[1] == y_1[0]
+
+    assert X_0[-1] == exp_tokens[max_len - 1]
+    assert y_0[-1] == exp_tokens[max_len]
+
+    assert X_1[-1] == exp_tokens[max_len + stride - 1]
+    assert y_1[-1] == exp_tokens[max_len + stride]
