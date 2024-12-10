2bc3292e7af493dd44961563f0d695239f43106d
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-12-10 17:20:07 +0000

    Add first basic test

--- src/llmz/components/attention.py
+++ src/llmz/components/attention.py
@@ -1,1 +1,9 @@
 """Attention blocks for transformer models."""
+
+from torch import nn
+
+
+class Attention(nn.Module):
+    """Basic attention block."""
+
+    pass
--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,1 +1,21 @@
-"""Tests for transformer attention blocks."""
+"""Tests for transformer."""
+
+import torch
+from torch import nn
+
+from llmz.components.attention import Attention
+
+
+def test_Attention_output_size():
+    batch_size = 2
+    chunk_size = 4
+    embedding_dim = 5
+    attention_out_dim = embedding_dim
+
+    tokens_batch = torch.ones(batch_size, chunk_size, dtype=torch.int32)
+    embeddings_batch = nn.Embedding(10, embedding_dim)(tokens_batch)
+
+    attention = Attention()
+    out_batch = attention(embeddings_batch)
+
+    assert out_batch.Size() == torch.Size(batch_size, chunk_size, embedding_dim)
