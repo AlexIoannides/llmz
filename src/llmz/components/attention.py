36a1b8612f127dd7fe0ebfa1a57c17eb76a94201
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-12-10 18:07:32 +0000

    Add model initialisation

--- src/llmz/components/attention.py
+++ src/llmz/components/attention.py
@@ -11,19 +11,41 @@ class MultiHeadAttention(nn.Module):
             self,
             dim_in: int,
             dim_out: int,
-            chunk_size: int,
+            context_size: int,
             n_heads: int = 1,
             dropout: float = 0.6,
             qkv_bias: bool = False
         ):
-        """Initialise model."""
+        """Initialise model.
+
+        Args:
+            dim_in: TODO
+            dim_out: TODO
+            context_size: TODO
+            n_heads: TODO. Defaults to 1.
+            dropout: TODO. Defaults to 0.6.
+            qkv_bias: TODO. Defaults to False.
+
+        Raises:
+            ModelConfigError: TODO
+
+        """
         super().__init__()
         if dim_out % n_heads != 0:
             raise ModelConfigError("dim_out % n_heads != 0")
 
+        self.dim_out = dim_out
+        self.n_heads = n_heads
+        self.dim_head = dim_out // n_heads  # // --> returns int
         self.W_query = nn.Linear(dim_in, dim_out, bias=qkv_bias)
         self.W_key = nn.Linear(dim_in, dim_out, bias=qkv_bias)
         self.W_value = nn.Linear(dim_in, dim_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(dim_out, dim_out)
+        self.dropout = nn.Dropout(dropout)
+
+        self.register_buffer(
+            "mask", torch.triu(torch.ones(context_size, context_size), diagonal=1)
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Execute the model's forward pass."""
--- tests/test_attention.py
+++ tests/test_attention.py
@@ -9,17 +9,17 @@ from llmz.components.attention import ModelConfigError, MultiHeadAttention
 
 def test_Attention_output_size():
     batch_size = 2
-    chunk_size = 4
+    context_size = 4
     embedding_dim = 5
     attention_dim = embedding_dim
 
-    tokens_batch = torch.ones(batch_size, chunk_size, dtype=torch.int32)
+    tokens_batch = torch.ones(batch_size, context_size, dtype=torch.int32)
     embeddings_batch = nn.Embedding(10, embedding_dim)(tokens_batch)
 
-    attention = MultiHeadAttention(embedding_dim, attention_dim, chunk_size)
+    attention = MultiHeadAttention(embedding_dim, attention_dim, context_size)
     out_batch = attention(embeddings_batch)
 
-    assert out_batch.size() == torch.Size((batch_size, chunk_size, embedding_dim))
+    assert out_batch.size() == torch.Size((batch_size, context_size, embedding_dim))
 
 
 def test_Attention_raises_exception_on_bad_config():
