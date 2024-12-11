fc400f0f74eab8b61cd287fe049d1bf4ae21569b
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-12-11 10:18:13 +0000

    Improve test coverage

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -7,19 +7,17 @@ from torch import nn
 from llmz.components.attention import ModelConfigError, MultiHeadAttention
 
 
-def test_Attention_output_size():
-    batch_size = 2
-    context_size = 4
-    embedding_dim = 5
-    attention_dim = embedding_dim
+@pytest.mark.parametrize("batch_size, context_size, dim_out", [(1, 2, 5), (2, 4, 3)])
+def test_Attention_output_size(batch_size: int, context_size: int, dim_out: int):
+    dim_in = 9
 
     tokens_batch = torch.ones(batch_size, context_size, dtype=torch.int32)
-    embeddings_batch = nn.Embedding(10, embedding_dim)(tokens_batch)
+    embeddings_batch = nn.Embedding(10, dim_in)(tokens_batch)
 
-    attention = MultiHeadAttention(context_size, embedding_dim, attention_dim)
+    attention = MultiHeadAttention(context_size, dim_in, dim_out)
     out_batch = attention(embeddings_batch)
 
-    assert out_batch.size() == torch.Size((batch_size, context_size, embedding_dim))
+    assert out_batch.size() == torch.Size((batch_size, context_size, dim_in))
 
 
 def test_Attention_head_dim():
@@ -31,6 +29,7 @@ def test_Attention_head_dim():
     assert MultiHeadAttention(6, 6, dim_out=6, n_heads=3).dim_head == 2
 
 
-def test_Attention_raises_exception_on_bad_config():
+@pytest.mark.parametrize("dim_out", [7, 5, 2])
+def test_Attention_raises_exception_on_bad_config(dim_out: int):
     with pytest.raises(ModelConfigError, match="dim_out % n_heads != 0"):
-        MultiHeadAttention(3, 5, 5, 3)
+        MultiHeadAttention(3, 5, dim_out, 3)
