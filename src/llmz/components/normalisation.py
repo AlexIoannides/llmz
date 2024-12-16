982b239dfd5208038a006203ff6bef0576f643c9
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-12-16 07:53:08 +0000

    Add skeleton class and test

--- src/llmz/components/normalisation.py
+++ src/llmz/components/normalisation.py
@@ -1,11 +1,27 @@
 """Normalisation operations."""
 
+import torch
 from torch import nn
 
 
 class LayerNormalisation(nn.Module):
-    """Layer normalisation."""
+    """Layer normalisation.
+
+    Normalises batches of input tensor to zero mean and unit variance.
+    """
 
     def __init(self):
         """Initialise module."""
         super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of module.
+
+        Args:
+            x: input tensors.
+
+        Returns:
+            Tensor-by-tensor normalised version of the inputs.
+
+        """
+        return x
--- tests/test_normalisation.py
+++ tests/test_normalisation.py
@@ -1,7 +1,23 @@
 """Test for normalisation modules."""
 
+import torch
+
 from llmz.components.normalisation import LayerNormalisation
 
 
 def test_LayerNormalisation():
-    pass
+    torch.manual_seed(42)
+    test_inputs_size = (3, 10)
+    inputs = torch.randn(test_inputs_size)
+
+    layer_norm = LayerNormalisation()
+    inputs_norm = layer_norm(inputs)
+
+    mean_inputs_norm = inputs_norm.mean(dim=-1, keepdim=True)
+    var_inputs_norm = inputs_norm.var(dim=-1, keepdim=True)
+
+    exp_mean_inputs_norm = torch.zeros(test_inputs_size)
+    exp_var_inputs_norm = torch.ones(test_inputs_size)
+
+    torch.testing.assert_close(mean_inputs_norm, exp_mean_inputs_norm)
+    torch.testing.assert_close(var_inputs_norm, exp_var_inputs_norm)
