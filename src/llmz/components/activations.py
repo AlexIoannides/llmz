c33db88d5781322eed434acd55b95563972f39a2
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-12-23 16:55:24 +0000

    Add implementation with basic test cases

--- src/llmz/components/activations.py
+++ src/llmz/components/activations.py
@@ -1,9 +1,33 @@
 """Activation functions for transformer models."""
 
+import torch
 from torch import nn
 
 
 class GELU(nn.Module):
-    """Guassian Error Linear Unit (GELU)."""
+    """Guassian Error Linear Unit (GELU).
 
-    pass
+    Implemented using an approximation to `x * F(x)`, where `F` is the cumulative
+    normal distribution function.
+    """
+
+    def __init__(self):
+        """Initialise module."""
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Execute the module's forward pass.
+
+        Args:
+            x: Batch of input tensors.
+
+        Returns:
+            Batch of output tensors that have been filtered on an element-by-element
+                basis using the GELU activation function.
+
+        """
+        tanh_exponent = torch.sqrt(
+            torch.tensor(2.0 / torch.pi) * (x + 0.044715 * torch.pow(x, 3))
+        )
+        gelu_x = 0.5 * x * (1.0 + torch.tanh(tanh_exponent))
+        return gelu_x
--- tests/test_activations.py
+++ tests/test_activations.py
@@ -1,7 +1,18 @@
 """Tests for custom activation functions."""
 
+import pytest
+import torch
+
 from llmz.components.activations import GELU
 
 
-def test_GELU():
-    pass
+@pytest.mark.parametrize(
+    "x, result",
+    [
+        (torch.tensor(0.), torch.tensor(0.)),
+        (torch.tensor([0., 0.]), torch.tensor([0., 0.]))
+    ]
+)
+def test_GELU(x: torch.Tensor, result: torch.Tensor):
+    gelu = GELU()
+    torch.testing.assert_close(gelu(x), result, atol=0., rtol=0.)
