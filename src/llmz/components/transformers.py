0d339fa386b4348298ccfd936d681d3f948a8b84
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-12-24 11:44:21 +0000

    Add function test and skeleton implementation

--- src/llmz/components/transformers.py
+++ src/llmz/components/transformers.py
@@ -1,9 +1,43 @@
 """Transformer block for LLMs."""
 
+import torch
 from torch import nn
 
 
 class TransformerBlock(nn.Module):
-    """Transformer block using multi-head attention."""
+    """Basic transformer block with multi-head attention."""
 
-    pass
+    def __init__(
+        self,
+        context_size: int,
+        dim_in: int,
+        dim_out: int,
+        n_heads: int = 1,
+        dropout: float = 0.6,
+        qkv_bias: bool = False,
+    ):
+        """Initialise module.
+
+        Args:
+            dim_in: Dimension of input word embeddings.
+            dim_out: Dimension of output attention embeddings.
+            context_size: The number of input word embeddings in teh sequence.
+            n_heads: The number of attention heads. Defaults to 1.
+            dropout: The dropout rate. Defaults to 0.6.
+            qkv_bias: Whether or not to include bias in the linear layers used to
+                compute W_query, W_key and W_value. Defaults to False.
+
+        """
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Execute the module's forward pass.
+
+        Args:
+            x: Batch of token embeddings.
+
+        Returns:
+            Batch of attention weighted embeddings.
+
+        """
+        return x
--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -1,8 +1,20 @@
 """Tests for transformer blocks."""
 
+import pytest
+import torch
+from torch import nn
+
 from llmz.components.transformers import TransformerBlock
 
 
-def test_TransformerBlock():
-    tsfmr = TransformerBlock()
-    assert True
+@pytest.mark.parametrize("batch_size, context_size, dim_out", [(1, 2, 5), (2, 4, 3)])
+def test_TransformerBlock_output_size(batch_size: int, context_size: int, dim_out: int):
+    dim_in = 9
+
+    tokens_batch = torch.ones(batch_size, context_size, dtype=torch.int32)
+    embeddings_batch = nn.Embedding(10, dim_in)(tokens_batch)
+
+    transformer = TransformerBlock(context_size, dim_in, dim_out)
+    out_batch = transformer(embeddings_batch)
+
+    assert out_batch.size() == torch.Size((batch_size, context_size, dim_out))
