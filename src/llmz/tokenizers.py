d1e3d61a8db654427a4da29eaf5b595bab3eba94
alexioannides <alex.ioanndies@yahoo.co.uk>
2025-02-10 07:54:04 +0000

    Add ABC for tokenizers

---
+++ src/llmz/tokenizers.py
@@ -0,0 +1,29 @@
+"""Tools for tokenizing text.
+
+Individual model definitions may have their own tokenizers defined alongside them,
+but they will all implement the interface defined here.
+"""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class _Tokenizer(ABC):
+    """Abstract base class for text tokenizers."""
+
+    def __call__(self, text: str) -> list[int]:
+        return self.text2tokens(text)
+
+    @abstractmethod
+    def train(self, **kwargs: dict[str, Any]) -> _Tokenizer:
+        pass
+
+    @abstractmethod
+    def text2tokens(self, text: str) -> list[int]:
+        pass
+
+    @abstractmethod
+    def tokens2text(self, token: list[int]) -> str:
+        pass
