8f173ed04400266da114c92f08c364683ec4ac20
alexioannides <alex.ioanndies@yahoo.co.uk>
2025-03-06 08:01:49 +0000

    Add test for greedy decoding algorithm

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -4,7 +4,7 @@
 import pytest
 import torch
 
-from llmz.generate import _sample_decoding, _top_k_decoding
+from llmz.generate import _greedy_decoding, _sample_decoding, _top_k_decoding
 
 
 @pytest.mark.parametrize(
@@ -46,3 +46,22 @@ def test_top_k_decoding(
     )
     token1_pct = token1_sampled.sum() / n_samples
     assert float(token1_pct) == pytest.approx(token1_expected_pct, abs=0.05)
+
+
+@pytest.mark.parametrize(
+    "logits, temperature, token1_expected_pct",
+    [(torch.tensor([1.0, 1.0]), 1.0, 0.0),
+     (torch.tensor([1.0, 10.0]), 1.0, 1.0),
+     (torch.tensor([1.0, 10.0]), 1000.0, 1.0),
+     (torch.tensor([1.0, 2.0]), 0.001, 1.0)]
+)
+def test_greedy_decoding(
+    logits: torch.Tensor, temperature: float, token1_expected_pct: int
+):
+    torch.manual_seed(42)
+    n_samples = 1000
+    token1_sampled = torch.tensor(
+        [_greedy_decoding(logits, temperature) == 1 for _ in range(n_samples)]
+    )
+    token1_pct = token1_sampled.sum() / n_samples
+    assert float(token1_pct) == pytest.approx(token1_expected_pct, abs==0.05)
