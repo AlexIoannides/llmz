62e37f2ae6e8a3a1776d8c716f6f7445c2b61629
alexioannides <alex.ioanndies@yahoo.co.uk>
2025-11-01 10:40:16 +0000

    Enable evals to compute on any device

--- src/llmz/evaluate.py
+++ src/llmz/evaluate.py
@@ -4,18 +4,23 @@ import logging
 from collections.abc import Callable, Iterator
 from typing import NamedTuple
 
+import torch
 from torch import nn
 from torch.nn import functional as f
 from torch.utils.data import DataLoader
 
-Result = float | int | str
+MetricResult = float | int
+MetricsFunc = Callable[[nn.Module, DataLoader, torch.device], dict[str, MetricResult]]
+
+ScenarioResult = float | int | str
+ScenariosFunc = Callable[[nn.Module, torch.device], dict[str, ScenarioResult]]
 
 
 class EvalResult(NamedTuple):
     """Container for evaluation results produced during training."""
 
     step: int
-    results: dict[str, Result]
+    results: dict[str, MetricResult | ScenarioResult]
 
 
 class Evaluator:
@@ -28,8 +33,9 @@ class Evaluator:
         self,
         train_dataloader: DataLoader,
         val_dataloader: DataLoader,
-        metrics_fn: Callable[[nn.Module, DataLoader], dict[str, Result]],
-        scenarios_fn: Callable[[nn.Module], dict[str, Result]] | None = None,
+        metrics_fn: MetricsFunc,
+        scenarios_fn: ScenariosFunc | None = None,
+        device: torch.device = torch.device("cpu"),
     ):
         """Initialise.
 
@@ -41,6 +47,7 @@ class Evaluator:
             scenarios_fn: Optional callable that returns a dictionary of results/outputs
                 given a model - e.g., generated text given an example prompt. Defaults
                 to None.
+            device: The processor to use for training. Defaults to CPU.
 
         """
         self.train_dl = train_dataloader
@@ -48,6 +55,7 @@ class Evaluator:
         self.metrics_fn = metrics_fn
         self.scenarios_fn = scenarios_fn
         self._eval_records: list[EvalResult] = []
+        self.device = device
 
     def evaluate(
         self, step: int, model: nn.Module, log: logging.Logger | None = None
@@ -64,12 +72,14 @@ class Evaluator:
 
         """
         train_metrics = {
-            f"train_{k}": v for k, v in self.metrics_fn(model, self.train_dl).items()
+            f"train_{k}": v
+            for k, v in self.metrics_fn(model, self.train_dl, self.device).items()
         }
         val_metrics = {
-            f"val_{k}": v for k, v in self.metrics_fn(model, self.val_dl).items()
+            f"val_{k}": v
+            for k, v in self.metrics_fn(model, self.val_dl, self.device).items()
         }
-        scenarios = self.scenarios_fn(model) if self.scenarios_fn else {}
+        scenarios = self.scenarios_fn(model, self.device) if self.scenarios_fn else {}
 
         eval_record = EvalResult(step, {**train_metrics, **val_metrics, **scenarios})
         self._eval_records.append(eval_record)
@@ -90,15 +100,21 @@ class Evaluator:
         return len(self._eval_records)
 
 
-def basic_llm_metrics(model: nn.Module, dl: DataLoader) -> dict[str, float]:
+def basic_llm_metrics(
+    model: nn.Module, dl: DataLoader, device=torch.device("cpu")
+) -> dict[str, float]:
     """Compute basic LLM metrics for a dataloader.
 
     Args:
         model: Model to use for inference.
         dl: Dataloader with data batches for inference.
+        device: The processor to use for training. Defaults to CPU.
 
     """
     loss = sum(
-        f.cross_entropy(model(X).flatten(0, 1), y.flatten()).item() for X, y in dl
+        f.cross_entropy(
+            model(X.to(device)).flatten(0, 1), y.to(device).flatten()
+        ).item()
+        for X, y in dl
     ) / len(dl)
     return {"loss": loss}
