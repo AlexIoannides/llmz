bb06e8c3c1a034a2913c2058b4e048f76460771d
alexioannides <alex.ioanndies@yahoo.co.uk>
2025-11-06 08:06:16 +0000

    Add initialisation test

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,7 +1,14 @@
 """Tests for utility functions."""
 
+from pathlib import Path
+from unittest.mock import patch
+
 from llmz.utils import LocalFSCheckpointHandler
 
 
-def test_LocalFSCheckpointHandler():
-    assert LocalFSCheckpointHandler is not None
+def test_LocalFSCheckpointHandler_creates_ckpt_dir(tmp_path: Path):
+    ckpt_base_name = "llmz"
+    with patch("llmz.utils.LOCAL_FS_PATH") as local_ckpt_dir:
+        local_ckpt_dir.return_value = tmp_path
+        LocalFSCheckpointHandler(ckpt_base_name)
+    assert (tmp_path / ckpt_base_name).exists()
